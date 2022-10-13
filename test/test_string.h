3391e0a067af3cf66306cf38be83d9192ad552b6
Baron Leonardo <mohamed.a.elmeligy@protonmail.com>
2022-10-13 12:00:30 +0200

    test trim functions

--- test/test_string.h
+++ test/test_string.h
@@ -8,6 +8,7 @@
 static inline void c_test_string(CUnit_Test* unit_test)
 {
     c_test_register(unit_test, c_test_string_new);
+    c_test_register(unit_test, c_test_string_trim);
 }
 
 void c_test_string_new(CUnit_Test* self)
@@ -19,6 +20,20 @@ void c_test_string_new(CUnit_Test* self)
 
     c_clog_info("String: %s", str);
     c_clog_info("String len: %d", c_string_len(str));
+
+    c_string_free(str);
+}
+
+void c_test_string_trim(CUnit_Test* self)
+{
+    char* buf = " my name is C\n";
+    cstr str = c_string_new_from_buf(buf, strlen(buf));
+
+    c_string_rtrim_whitespaces(str);
+    TEST_CHECK(c_string_equal(str, " my name is C"));
+
+    c_string_ltrim_whitespaces(str);
+    TEST_CHECK(c_string_equal(str, "my name is C"));
 
     c_string_free(str);
 }
