c341bdd77e0eb83ba8aae3fca096da263449eac2
Mohamed A. Elmeligy <mohamed.a.elmeligy@protonmail.com>
2023-09-07 05:53:11 +0300

    log: use safer localtime

--- log/log_internal.h
+++ log/log_internal.h
@@ -8,13 +8,20 @@
 static inline void
 log_internal_get_cur_time(char* time_buf, size_t time_buf_size) {
     time_t rawtime;
-    struct tm* timeinfo;
+    struct tm timeinfo;
 
     time(&rawtime);
+
+#if defined(__unix__)
+    localtime_r(&rawtime, &timeinfo);
+#elif defined(_MSC_VER) && !defined(__INTEL_COMPILER)   // MSVC on windows
+    localtime_s(&timeinfo, &rawtime);
+#else
     timeinfo = localtime(&rawtime);
     cassert(timeinfo);
+#endif
 
-    cassert(strftime (time_buf, time_buf_size, "%H:%M:%S", timeinfo));
+    cassert(strftime (time_buf, time_buf_size, "%H:%M:%S", &timeinfo));
     time_buf[time_buf_size - 1] = '\0';
 }
 
