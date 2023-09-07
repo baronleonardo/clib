291a8aea85bbe4024c9c03c4360d585e614c2ec9
Mohamed A. Elmeligy <mohamed.a.elmeligy@protonmail.com>
2023-09-07 23:50:42 +0300

    common: add cassert_always macros

--- common/cassert.h
+++ common/cassert.h
@@ -1,11 +1,20 @@
 #ifndef CASSERT_H
 #define CASSERT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifdef NDEBUG
 #define cassert(expr) ((void)(expr))
 #else
 #include <assert.h>
 #define cassert(expr) assert((expr))
 #endif // NDEBUG
+
+/// @brief these macros will always run in Debug/Release modes
+#define __cassert_always_print(expr) (fprintf(stderr, "%s:%d: %s: Assertion `(%s)' failed.\n", __FILE__, __LINE__, __func__, #expr))
+#define cassert_always(expr) ((expr) ? 0 : (__cassert_always_print((expr)), abort()))
+#define cassert_always_msg(expr, msg) ((expr) ? ((void)0) : (__cassert_always_print(expr), puts((msg)), abort()))
+#define cassert_always_perror(expr, msg) ((expr) ? ((void)0) : (__cassert_always_print(expr), perror((msg)), abort()))
 
 #endif // CASSERT_H
