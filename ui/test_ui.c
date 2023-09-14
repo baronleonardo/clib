4f6f0e3e01edfd3be2c9524985745eb7f5983b50
Mohamed A. Elmeligy <mohamed.a.elmeligy@protonmail.com>
2023-09-13 21:16:15 -0700

    ui: expermenting with windows api

--- ui/test_ui.c
+++ ui/test_ui.c
@@ -1,29 +1,216 @@
-#include <ui.h>
+// #include <ui.h>
 
-#undef NDEBUG
-#include <assert.h>
+// #undef NDEBUG
+// #include <assert.h>
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// #define STR(str) (str), (sizeof((str)) - 1)
+
+// void
+// on_button_clicked_event(UiButton button, void* extra_data) {
+//     (void)button;
+//     (void)extra_data;
+
+//     puts("clicked");
+// }
+
+// void
+// on_activate_handler(Ui self) {
+//     UiButton* button = ui_button_create(self, STR("button"));
+//     ui_button_event_clicked(button, on_button_clicked_event, NULL);
+//     ui_child_add(self, button);
+// }
+
+// int main(int argc, char* argv[]) {
+//     Ui ui = ui_create(STR("This is a title"));
+//     ui_mainloop(ui, argv, argc, on_activate_handler);
+//     ui_destroy(&ui);
+// }
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
+#include <stddef.h>
+#include <assert.h>
 
-#define STR(str) (str), (sizeof((str)) - 1)
+#include <windows.h>
+#include <windowsx.h>
 
-void
-on_button_clicked_event(UiButton button, void* extra_data) {
-    (void)button;
-    (void)extra_data;
+static LRESULT __stdcall windowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-    puts("clicked");
+int main(int argc, char** argv){
+    //---- Get WinMain Parameters ----//
+
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    STARTUPINFO si;
+    GetStartupInfo(&si);  
+    int nCmdShow = si.wShowWindow;
+
+    // -------------------------//
+    OutputDebugString("Starting WinMain Application");
+    puts("Starting WinMain Application");
+    
+    //Window class name must be unique 
+    const char wincClassName [] = "NameOfWindow";
+    
+    // Win32 Window class structure
+    WNDCLASSEX wc = {0};
+    // Win32 message structure 
+    MSG Msg;        	
+    
+    wc.lpszClassName = wincClassName;
+    wc.lpfnWndProc = &windowProcedure;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    // wc.style  = 0;
+    // wc.cbClsExtra = 0;
+    // wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    // wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = GetSysColorBrush(COLOR_WINDOW);
+    // wc.lpszMenuName = NULL;	
+    // wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);  
+
+    if(!RegisterClassEx(&wc)) {
+        MessageBox(NULL,
+                   "Window Registration Failed!",
+                   "Error!",
+                   MB_ICONEXCLAMATION | MB_OK);
+        //Error status code 
+        return -1;
+    }
+
+    puts("Class Registered");
+    int width = 500, height = 400;
+    int pos_left = 400, pos_top = 100;
+
+    HWND hwnd = CreateWindowA(
+        wc.lpszClassName,
+        "Title of Window",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        pos_left,
+        pos_top,
+        width,
+        height,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+        );
+    
+    const size_t buttonID = 1001;
+    
+    OutputDebugString(" [INFO] Window created OK");
+    if(hwnd == NULL){
+        MessageBox(NULL,
+                   "Error: Failure to create Window",
+                   "Error Report",
+                   MB_ICONEXCLAMATION | MB_OK);
+        return -1;
+    }
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    //---- Message Loop ----------//
+    while(GetMessage(&Msg, NULL, 0, 0) > 0 ){
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }	
+    // Success status code 
+    return 0;
 }
+// ------------ End of Main ------------------------- //
 
-void
-on_activate_handler(Ui self) {
-    UiButton* button = ui_button_create(self, STR("button"));
-    ui_button_event_clicked(button, on_button_clicked_event, NULL);
-    ui_child_add(self, button);
-}
 
-int main(int argc, char* argv[]) {
-    Ui ui = ui_create(STR("This is a title"));
-    ui_mainloop(ui, argv, argc, on_activate_handler);
-    ui_destroy(&ui);
+// Window Procedure - Process window messages or events 
+LRESULT __stdcall windowProcedure (
+     HWND   hwnd    // Window Handle (Window object)
+    ,UINT   msg     // Window Message  
+    ,WPARAM wParam  // Additional message information
+    ,LPARAM lParam  // Additional message information
+    ){
+
+    // Process messages 
+    switch(msg)
+    {
+    case WM_CREATE:
+        LONG units = GetDialogBaseUnits();
+        CreateWindow( 
+            "BUTTON",  // Predefined class; Unicode assumed 
+            "OK",      // Button text 
+            // WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+            BS_PUSHBUTTON | WS_VISIBLE | WS_CHILD | WS_TABSTOP,
+            CW_USEDEFAULT,         // x position 
+            CW_USEDEFAULT,         // y position 
+            MulDiv(LOWORD(units), 50, 4),        // Button width with default size
+            MulDiv(HIWORD(units), 14, 8),        // Button height with default size
+            hwnd,     // Parent window
+            NULL,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+            // NULL,
+            NULL // Pointer not needed.
+        );
+
+        assert(CreateWindow( 
+            "EDIT",  // Predefined class; Unicode assumed 
+            NULL,      // Button text 
+            // WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+            WS_BORDER | WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL,
+            100,         // x position 
+            100,         // y position 
+            MulDiv(LOWORD(units), 50, 4),         // Button width with default size
+            MulDiv(HIWORD(units), 14, 8),         // Button height with default size
+            hwnd,      // Parent window
+            NULL,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+            // NULL,
+            NULL // Pointer not needed.
+        ));
+        break;
+    case WM_CLOSE:		
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_MOVE:
+        break;
+    case WM_COMMAND:    
+        switch(HIWORD(wParam)) {
+            case BN_CLICKED:
+                puts("clicked");
+                break;
+            case EN_UPDATE:
+                char buf[2000];
+                Edit_GetText((HWND)lParam, buf, 2000);
+                puts(buf);
+                break;
+            default: 
+                return DefWindowProc(hwnd, msg, wParam, lParam);
+        }
+        break;
+    case WM_PAINT:
+    // {
+    // 	// GDI - Graphics Devices Interface Here
+    // 	//--------------------------------------------
+    // 	PAINTSTRUCT ps;
+    // 	HDC hdc;
+    // 	// std::cerr << " [INFO] Windown painting" << std::endl;
+    // 	hdc = BeginPaint(hwnd, &ps);
+    // 	const char* text = "Hello world Window!";		
+    // 	TextOutA(hdc, 125, 200, text, sizeof(text) - 1);
+    // 	Ellipse(hdc, 100, 100, 160, 160);
+    // 	Rectangle(hdc, 100, 100, 160, 160);
+    // 	EndPaint(hwnd, &ps);	
+    // }
+    break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
 }
---
+++ ui/ui_internal_win.c
@@ -0,0 +1,45 @@
+#include <ui_internal_win.h>
+
+UiBackend
+ui_internal_win_create(const char* title, size_t title_len) {
+    
+}
+
+
+void
+ui_internal_win_child_add(UiBackend self, UiBackendWidget* widget) {
+    
+}
+
+
+UiBackendButton*
+ui_internal_win_button_add(UiBackend self, const char* label, size_t label_len) {
+    
+}
+
+
+void
+ui_internal_win_button_event_clicked(
+    UiBackendButton* button,
+    void on_click_event(UiBackendButton* button, void* extra_data),
+    void* extra_data
+) {
+    
+}
+
+
+void
+ui_internal_win_mainloop(
+    UiBackend self,
+    char* argv[],
+    int argc,
+    void on_activate_handler(UiBackend self)
+) {
+    
+}
+
+
+void
+ui_internal_win_destroy(UiBackend* self) {
+    
+}
